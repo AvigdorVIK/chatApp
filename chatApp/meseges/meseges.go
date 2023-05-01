4de00d0465ac7f069415e57ccbc54f7236a49895
AvigdorVIK <kanynnikovvik19@gmail.com>
2023-05-01 14:48:56 +0300

    meseges

---
+++ chatApp/meseges/meseges.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	MsgInvalidCredentials  = "Invalid email or password"
+	MsgUserNotFound        = "User not found"
+	MsgInternalServerError = "Internal Server Error"
+	MsgMissingField        = "Missing required field"
+	MsgDuplicateEmail      = "Email already exists"
+	MsgInvalidEmail        = "Invalid email address"
+	MsgPasswordTooShort    = "Password must be at least 6 characters long"
+	MsgPasswordsDoNotMatch = "Passwords do not match"
+	MsgInvalidName         = "Invalid name"
+)
